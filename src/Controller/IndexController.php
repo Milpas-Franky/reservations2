f462a0922f6ac9d69a104cc574c864be51592243
kaponda <stefkap11@gmail.com>
2022-05-01 12:56:13 +0200

    fin configuration controller index

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Shows;
+class IndexController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_index)
+     */
+    public function index(): Response
+    {
+        $repository = $this->getDoctrine()->getRepository(Shows::class);
+        $shows = $repository->findAll();
+        
+        return $this->render('index/index.html.twig', [
+            'shows' => $shows,
+            'resource' => 'spectacles',
+        ]);
+    }
+	
+	
+}
