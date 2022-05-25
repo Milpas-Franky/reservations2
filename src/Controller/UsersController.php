99036b6caa4917bce3f1779b5fbce6b91c9c91a8
Milpas Franky <https://github.com/FMilpas/reservations2.git>
2022-05-25 20:08:36 +0200

    templates _form,delete,edit,index,new

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -7,11 +7,17 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Users;
 use Symfony\Component\HttpFoundation\Request;
+use App\Repository\UsersRepository;
+use App\Form\UsersType;
 
 class UsersController extends AbstractController
 {
-    #[Route('/users', name: 'app_users')]
-    public function index(): Response
+    /** 
+    * @Route("/users")"/users", name= "app_users")
+    * @param UsersRepository $usersRepository
+     * @return Response
+     */
+    public function index(UsersRepository $usersRepository): Response
     {
 
         $repository = $this->getDoctrine()->getRepository(Users::class);
@@ -21,5 +27,87 @@ class UsersController extends AbstractController
             'users' => $users,
             
         ]);
+    }
+
+    
+    /**
+     * @Route("/new", name="users_new", methods={"GET","POST"})
+     * @param Request $request
+     * @return Response
+     */
+
+    public function new(Request $request): Response
+    {
+        $user = new Users();
+        $form = $this->createForm(UsersType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_users');
+        }
+
+        return $this->render('users/new.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    
+    /**
+     * @Route("/{id}", name="users_show", methods={"GET"})
+     * @param Users $user
+     * @return Response
+     */
+    public function show(Users $user): Response
+    {
+        return $this->render('users/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="users_edit", methods={"GET","POST"})
+     * @param Request $request
+     * @param Users $user
+     * @return Response
+     */
+
+    public function edit(Request $request, Users $user): Response
+    {
+        $form = $this->createForm(UsersType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('app_users');
+        }
+
+        return $this->render('users/edit.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="users_delete", methods={"DELETE"})
+     * @param Request $request
+     * @param Users $user
+     * @return Response
+     */
+
+    public function delete(Request $request, Users $user): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($user);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_users');
     }
 }
---
+++ src/Form/UsersType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Users;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UsersType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('password')
+            ->add('firstname')
+            ->add('lastname')
+            ->add('username')
+            ->add('langue')
+            ->add('roles')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Users::class,
+        ]);
+    }
+}
